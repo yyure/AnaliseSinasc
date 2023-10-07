a5cafcb9fda0f1e80bf4cf0bc8414529a9852cb4
Yure Santos de Oliveira <yure.sdo@gmail.com>
2023-10-07 15:20:43 -0300

    Atualize main.py

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 import pandas as pd
-import modules 
+import numpy as np
+from modules import cleaning
 
 def main():
-    caminho_arquivo = 'SINASC_2021.csv'
-
-    df = pd.read_csv(caminho_arquivo, encoding="unicode_escape", engine="python", sep=";", iterator=True, chunksize=1000)
-    for chunk in df:
-        print(chunk.head())
-
-    colunas_para_remover = modules.files_to_list('columns_to_remove.txt')
-
-    df.drop(columns=colunas_para_remover, inplace=True)
-
-    print(df)
+    cleaning.load_data('data/SINASC_2021.csv', 'data/saida.csv')
+    df = pd.read_csv('data/saida.csv', encoding="unicode_escape", engine="python", sep=";")
+    print(df.head())
 
 if __name__ == "__main__":
     main()
-    
