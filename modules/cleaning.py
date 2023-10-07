9fdca88bb4ad266b7451a2ad781d43c68f326a79
Yure Santos de Oliveira <yure.sdo@gmail.com>
2023-10-07 13:03:38 -0300

    Adicione a função filter_rows

--- modules/cleaning.py
+++ modules/cleaning.py
@@ -1,17 +1,18 @@
 import pandas as pd
 
 def file_to_list(path: str) -> list[str]:
-    """Função que transforma as linhas de um arquivo .txt em uma lista
+    """Função que recebe um arquivo .txt e retorna uma lista em que
+    cada linha do arquivo é um elemento da lista.
 
     Parameters
     ----------
     path : str
-        Endereço do arquivo
+        Endereço do arquivo.
 
     Returns
     -------
     list[str]
-        Lista com as linhas do arquivo
+        Lista com as linhas do arquivo.
     """
 
     txt_list = []
@@ -27,3 +28,37 @@ def file_to_list(path: str) -> list[str]:
         print("Arquivo não encontrado.")
 
     return txt_list
+
+
+def filter_rows(df: pd.DataFrame, restrictions: dict) -> pd.DataFrame:
+    """Filtra as linhas de um DataFrame com base em um conjunto de restrições
+    para cada campo a ser verificado. Retorna um DataFrame somente com as linhas
+    que satisfazem todas as restrições.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame a ser filtrado.
+    restrictions : dict
+        Dicionário em que cada chave é uma coluna do DataFrame e o valor é uma
+        lista com os valores aceitos para aquela coluna.
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame somente com as linhas que satisfazem as restrições.
+    """
+
+    for restriction in restrictions:
+        # Nome da coluna e valores aceitos para a coluna
+        column = restriction
+        subset = restrictions[column]
+
+        try:
+            # Encontra as linhas válidas e filtra o DataFrame
+            valid_rows = df[column].isin(subset)
+            df = df[valid_rows]
+        except KeyError:
+            print("Coluna inválida.")
+    
+    return df
