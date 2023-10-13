7f31b2ed39b1b30cccc49ff07237830788033ce3
Henzo <henzofelipe1@gmail.com>
2023-10-13 16:42:36 -0300

    Comparação CORMAE e CORBEBE feita

--- modules/analise1.py
+++ modules/analise1.py
@@ -8,42 +8,7 @@ import doctest
 import config1
 import cleaning
 
-
-def filter_data_1(path_input: str, path_output: str):
-
-    # Verifica se o arquivo de configuração existe, caso contrário gera o arquivo
-    config_file_path = 'config1.yaml'
-    if not os.path.exists(config_file_path):
-        config1.generate_config_file(config_file_path)
-
-    # Carrega os dados do arquivo de configuração
-    with open(config_file_path, 'r') as file:
-        config_data = yaml.safe_load(file)
-
-    df_index = config_data['df_index']
-    columns_to_remove = config_data['columns_to_remove']
-
-    # Abre os dados pré-filtrados
-    try:
-        df = pd.read_csv(path_input, encoding="unicode_escape", engine="python", sep=";", iterator=True, chunksize=100000)
-    except FileNotFoundError:
-        print(f"Erro: Arquivo {path_input} não encontrado.")
-        return
-    
-    # Filtra os dados para esta analise
-    for chunk in df:
-        try:
-            chunk.set_index(df_index, inplace=True)
-        except KeyError:
-            print(f"Erro: coluna {df_index} não encontrada.")
-
-         # Remove as colunas que não serão utilizadas nesta analise
-        chunk.drop(columns=columns_to_remove, inplace=True, errors="ignore")
-
-        # Salva o DataFrame no novo arquivo de saída
-        chunk.to_csv(path_output, mode='a', sep=';')
-
-def coleta_dados(path_input: str):
+def analise_1_0(path_input: str):
 
     # Abre os dados filtrados
     try:
@@ -52,26 +17,77 @@ def coleta_dados(path_input: str):
         print(f"Erro: Arquivo {path_input} não encontrado.")
         return
     
-    end = pd.DataFrame([])
+    # Dataframe que contará as frequências
+    data_set = pd.DataFrame(data = None, index = [1, 2, 3, 4, 5], columns = ['RACACOR', 'RACACORMAE'])
+    data_set.fillna(0, inplace=True)
+
+    # Índice usado na iteração
+    RACACOR_index = ['1', '2', '3', '4', '5']
+
+    # Itera sobre os chunks
     for chunk in df:
-        chunk = chunk.loc[['RACACOR', 'RACACORMAE']]
-        end.add(chunk.value_counts(), fill_value = 0)
 
-    end = end.groupby(level = 0).sum()
-    end['RACACOR'] /= end['RACACOR'].sum
-    end['RACACORMAE'] /= end['RACACORMAE'].sum
+        try:
+            chunk = chunk[['RACACOR', 'RACACORMAE']]
+        except KeyError:
+            print('Erro: arquivo não possui colunas \'RACACOR\' ou \'RACACORMAE\'.')
+            return data_set
+        
+        # Itera sobre as cores
+        for COR in RACACOR_index:
+            filtro = chunk['RACACOR'] == COR
+            count_cor_filho = len(chunk.loc[filtro])
 
+            filtro = (chunk['RACACORMAE'] == COR)
+            count_cor_mae = len(chunk.loc[filtro])
+
+            data_set.loc[int(COR), 'RACACOR'] += count_cor_filho
+            data_set.loc[int(COR), 'RACACORMAE'] += count_cor_mae
+
+    # Normaliza os valores percentualmente
+    data_set.iloc[:, :] = data_set.iloc[:, :].apply(lambda x: x/x.sum(), axis=0)
+
+    #Plota os gráficos lado a lado
     fig, axs = plt.subplots(1, 2, sharey = True, tight_layout = True)
+    X_label = ['Branco', 'Preto', 'Amarelo', 'Pardo', 'Indigena']
+    cores = ['tab:red', 'tab:red', 'tab:red', 'tab:red', 'tab:red']
 
-    axs[0].hist(end['RACACOR'], bins = 5)
-    axs[1].hist(end['RACACORMAE'], bins = 5)
+    axs[0].set_ylabel('Frequência')
+    axs[0].set_title('Cor do bebê')
+    axs[1].set_title('Cor da mãe')
+
+    axs[0].bar(X_label, data_set['RACACOR'])
+    axs[1].bar(X_label, data_set['RACACORMAE'], color=cores)
 
     plt.show()
 
-    diff = end['RACACORMAE'] - end['RACACOR']
-
-    plt.hist(diff)
+    #Plota a diferença de cor da mãe e do filho
+    diff = data_set['RACACORMAE'] - data_set['RACACOR']
+    
+    fig, axs = plt.subplots()
+    axs.set_title('Porcentagem da diferença de cores')
+    axs.bar(X_label, diff*100)
+    
     plt.show()    
 
+def analise_1_2(path_input: str):
+    
+    # Abre os dados filtrados
+    try:
+        df = pd.read_csv(path_input, encoding="unicode_escape", engine="python", sep=";", iterator=True, chunksize=100000)
+    except FileNotFoundError:
+        print(f"Erro: Arquivo {path_input} não encontrado.")
+        return
+    
+    # Dataframe que contará as frequências
+    data_set = pd.DataFrame(data = None, index = [1, 2, 3, 4, 5], columns = ['RACACOR', 'RACACORMAE'])
+    data_set.fillna(0, inplace=True)
+
+    # Índice usado na iteração
+    RACACOR_index = ['1', '2', '3', '4', '5']
+
+    # Itera sobre os chunks
+    
+
 if __name__ == "__main__":
-    coleta_dados('../data/saida.csv')
+    analise_1_0('../data/saida.csv')
