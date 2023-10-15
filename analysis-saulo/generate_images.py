a0721e50c96411258a915e023f0ed85359ffff91
Saulo Pires <saulopiresvieira5@gmail.com>
2023-10-15 10:41:34 -0300

    Adicione alterações nas imagens geradas

--- analysis-saulo/generate_images.py
+++ analysis-saulo/generate_images.py
@@ -20,14 +20,17 @@ df = df[["CODMUNNASC", column_name1, column_name2]]
 state_data = analysis.separate_by_location(df, state_mapping)
 region_data = analysis.separate_by_location(df, region_mapping)
 
+# Tabela das médias (CONSPRENAT)
+analysis.calculate_and_save_region_averages(region_data, column_name2, "data_analise/average_region.CONSPRENAT.csv")
+
 # Imagem 1: Gráfico de Barras para Regiões (CONSPRENAT)
-output_path_consprenat = 'data_analise/images/bar_plot_region.png'
-generate_bar(region_data, column_name1, "Regiões", f"Média de {column_name2} por Região", output_path_consprenat)
+output_path_1 = 'data_analise/images/bar_plot_region.png'
+generate_bar(region_data, column_name2, "Regiões", f"Média de {column_name2} por Região", output_path_1)
 
 # Imagem 2: Boxplot para Regiões (CONSPRENAT)
-output_path_kotelchuck = 'data_analise/images/boxplot_region.png'
-generate_boxplot(region_data, column_name1, "Regiões", f"Distribuição de {column_name2} por Região", output_path_kotelchuck)
+output_path_2 = 'data_analise/images/boxplot_region.png'
+generate_boxplot(region_data, column_name2, "Regiões", f"Distribuição de {column_name2} por Região", output_path_2, 20)
 
 # Teste 3: Mapa de calor cpara estados (KOTELCHUCK)
-output_path_heatmap = 'data_analise/images/heatmap.png'
-generate_heatmap(state_data, column_name1, shapefile_path, output_path_heatmap)
+output_path_3 = 'data_analise/images/heatmap.png'
+generate_heatmap(state_data, column_name1, shapefile_path, output_path_3)
